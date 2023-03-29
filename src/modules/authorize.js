49e8bad98bbdd5becdb8e23b2ee82f7e19464ad6
Duncan Regan <duncanregan@gmail.com>
2023-03-28 22:18:15 -0400

    Update the redirect url

--- src/modules/authorize.js
+++ src/modules/authorize.js
@@ -3,7 +3,7 @@ const AUTH_URL = 'https://smartweather.weatherflow.com/authorize.html'
 export const toAuthUrl = ({ clientId: client_id, codeChallenge: code_challenge }) => {
   const params = {
     response_type: 'code',
-    redirect_uri: 'localhost:8000',
+    redirect_uri: 'https://snowtires.pages.dev',
     code_challenge_method: 'S256',
     client_id,
     code_challenge,
