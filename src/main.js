eb5adfefbb7786967861d9f5022836abff976c78
Duncan Regan <duncanregan@gmail.com>
2023-03-30 11:00:52 -0400

    Cleanin up

--- src/main.js
+++ src/main.js
@@ -5,16 +5,15 @@ import { toToken } from './modules/token.js'
 const CLIENT_ID = '2f36eae2-d93f-4a65-a258-b3bedca38321'
 
 ;(async () => {
-  const codeVerifier = await genVerifier((x) => x)
+  const codeVerifier = genVerifier()
   const codeChallenge = await toCodeChallenge(codeVerifier)
   const authUrl = toAuthUrl({ clientId: CLIENT_ID, codeChallenge })
-  console.log(codeVerifier, codeChallenge)
+
   document.getElementById('authorize').setAttribute('href', authUrl)
+
   const code = new URLSearchParams(window.location.search).get('code') ?? ''
   if (code) {
     const token = await toToken({ clientId: CLIENT_ID, codeVerifier, code })
     console.log(token)
-  } else {
-    console.log('nope', code)
   }
 })()
