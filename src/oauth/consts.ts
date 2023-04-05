d556e346ead019a1a20a062b2e09da1f772e2a1d
Duncan Regan <duncanregan@gmail.com>
2023-04-05 18:59:44 -0400

    Move auth url to consts

--- src/oauth/consts.ts
+++ src/oauth/consts.ts
@@ -1,5 +1,7 @@
 export const CLIENT_ID = '2f36eae2-d93f-4a65-a258-b3bedca38321'
 
+export const AUTH_URL = 'https://smartweather.weatherflow.com/authorize.html'
+
 export const TOKEN_URL = 'https://swd.weatherflow.com/id/oauth2/token'
 
 export const TOKEN_KEY = 'authToken'
