f606be210cec3a3b859387a36c155ab4c1673c67
Duncan Regan <duncanregan@gmail.com>
2023-04-11 12:54:39 -0400

    Update homepage to use components

--- src/modules/homePage.ts
+++ src/modules/homePage.ts
@@ -1,19 +1,2 @@
-import { toAuthUrl } from '../oauth/authorize.js'
-import { genVerifier, toCodeChallenge } from '../oauth/codeVerifier.js'
-import { CLIENT_ID, TOKEN_KEY } from '../oauth/consts.js'
-import { redirect } from '../utils/navigate.js'
-import { read } from '../utils/storage.js'
-
-const token = read(TOKEN_KEY)
-if (token) {
-  redirect('/tires')
-}
-
-const codeVerifier = genVerifier(60)
-const codeChallenge = await toCodeChallenge(codeVerifier)
-const authUrl = toAuthUrl({
-  clientId: CLIENT_ID,
-  codeChallenge,
-  state: codeVerifier,
-})
-document.getElementById('authorize')?.setAttribute('href', authUrl)
+import '../components/authorize-link.js'
+import '../components/redirect-authorized.ts'
